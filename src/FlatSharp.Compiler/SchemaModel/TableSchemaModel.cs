9c11c0df69ddc4240925f8f0c2dae823a9fd383c
Alon Talmi <alon@sparkgames.com>
2026-02-19 14:27:23 +0200

    Removed self referencing DebuggerTypeProxy

--- src/FlatSharp.Compiler/SchemaModel/TableSchemaModel.cs
+++ src/FlatSharp.Compiler/SchemaModel/TableSchemaModel.cs
@@ -97,7 +97,6 @@ public class TableSchemaModel : BaseReferenceTypeSchemaModel
         writer.AppendLine(attribute);
         this.Attributes.EmitAsMetadata(writer);
         writer.AppendLine("[System.Runtime.CompilerServices.CompilerGenerated]");
-        writer.AppendLine($"[System.Diagnostics.DebuggerTypeProxy(\"{this.FullName}\")]");
         writer.AppendLine($"public partial class {this.Name}");
 
         using (writer.IncreaseIndent())
